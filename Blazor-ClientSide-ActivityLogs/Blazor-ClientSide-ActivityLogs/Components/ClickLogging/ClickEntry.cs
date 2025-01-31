595b97568054e8fe3691b35870e1407595f8cd9e
Reinout Wijnholds <ReinoutWijnholds2002@gmail.com>
2025-01-31 12:00:23 +0100

    Add pop-up to show click events

--- Blazor-ClientSide-ActivityLogs/Blazor-ClientSide-ActivityLogs/Components/ClickLogging/ClickEntry.cs
+++ Blazor-ClientSide-ActivityLogs/Blazor-ClientSide-ActivityLogs/Components/ClickLogging/ClickEntry.cs
@@ -1,11 +1,18 @@
-﻿namespace Blazor_ClientSide_ActivityLogs.Components.ClickLogging;
+﻿using System.Text.Json.Serialization;
+
+namespace Blazor_ClientSide_ActivityLogs.Components.ClickLogging;
 
 public class ClickEntry
 {
+    [JsonPropertyName("route")]
     public string? Route { get; set; }
+    [JsonPropertyName("class")]
     public string? Class { get; set; }
+    [JsonPropertyName("x")]
     public double X { get; set; }
+    [JsonPropertyName("y")]
     public double Y { get; set; }
+    [JsonPropertyName("time")]
     public DateTime Time { get; set; }
 }
 
